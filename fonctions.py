a104014d4f67d9de83e5fdb68e0ea11adac4166c
Florian Goutieras <florian.goutiras@etu.umontpellier.fr>
2023-12-05 00:14:45 +0100

    Modification des fonctions pour calculer le SPR directement

--- fonctions.py
+++ fonctions.py
@@ -1,3 +1,4 @@
+# Pour un seeding donné en entrée, la fonction renvoie le "tier" de seeding (9ème, 13ème, 17ème ...)
 def seed_round(seed):
     tier=[1,2,3,4,5,7,9,13,17,25,33,49,65,97,129,193,257,385,513,769,1025]
     if(seed>1024):
@@ -5,17 +6,18 @@ def seed_round(seed):
         return()
     for i in range (len(tier)-1):
         if seed < tier[i+1]:
-            print("seed: ",seed, " tier: ",tier[i])
             return(tier[i])
 
+# Calcul du SPR (Seed Performance Rating): comparaison du placement et du tier de seeding
 def spr(seed,perf):
     tier=[1,2,3,4,5,7,9,13,17,25,33,49,65,97,129,193,257,385,513,769,1025]
-    if (seed not in tier) or (perf not in tier):
+    true_seed = seed_round(seed)
+    if (true_seed not in tier) or (perf not in tier):
         print("Erreur dans le seed ou le placement")
     else:
         for i in range (len(tier)):
-            if seed == tier[i]:
+            if true_seed == tier[i]:
                 seed_r = i
             if perf == tier[i]:
                 perf_r = i
-    print("SPR: ",seed_r-perf_r)
+    return(seed_r-perf_r)
